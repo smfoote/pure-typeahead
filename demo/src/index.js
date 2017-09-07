7cf6fde7a0c7bcf21001cb31ef320f1d6aaf33a6
Steven <smfoote@gmail.com>
2017-09-07 15:28:48 -0600

    Use the Typeahead components in the demo (#2)
    These components have not yet been created. This is just to show what
    the usage of these components will look like.

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,7 +7,13 @@ class Demo extends Component {
   render() {
     return <div>
       <h1>pure-typeahead Demo</h1>
-      <Example/>
+
+      <Typeahead>
+        <TypeaheadInput onChange={()=>{}}/>
+        <TypeaheadResultsList>
+          <TypeaheadResult onSelect={()=>{}}></TypeaheadResult>
+        </TypeaheadResultsList>
+      </Typeahead>
     </div>
   }
 }
