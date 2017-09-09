78588526310b394adc0bef4c32a1ce5e5a866fe7
Steven Footea <sfoote@linkedin.com>
2017-09-09 11:29:12 -0600

    Wire up keydown event to navigate the list

--- src/Typeahead.js
+++ src/Typeahead.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 
 import TypeaheadResultsList from './TypeaheadResultsList';
+import TypeaheadInput from './TypeaheadInput';
 
 export default class Typeahead extends Component {
   constructor(props) {
@@ -10,12 +11,21 @@ export default class Typeahead extends Component {
     };
   }
 
+  navigateList(dir) {
+    this.resultsList.navigateList(dir);
+  }
+
   render() {
     const children = React.Children.map(this.props.children, child => {
       if (child) {
         if (child.type === TypeaheadResultsList) {
           return React.cloneElement(child, {
-            highlightedIndex: this.state.highlightedIndex
+            highlightedIndex: this.state.highlightedIndex,
+            ref: (ref => this.resultsList = ref)
+          });
+        } else if (child.type === TypeaheadInput) {
+          return React.cloneElement(child, {
+            arrowKeyPressed: this.navigateList.bind(this)
           });
         }
       }
