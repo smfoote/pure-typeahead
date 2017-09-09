2f0d5078966d257ae2c6d8800bf1f156ee0648b3
Steven Footea <sfoote@linkedin.com>
2017-09-09 11:11:02 -0600

    Count results in TypeaheadResultsList

--- src/TypeaheadResultsList.js
+++ src/TypeaheadResultsList.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react'
+import TypeaheadResult from './TypeaheadResult'
 
 export default class TypeaheadResultsList extends Component {
+
+  countResults() {
+    let resultsCount = 0;
+    React.Children.forEach(this.props.children, child => {
+      if (child && child.type === TypeaheadResult) {
+        resultsCount++;
+      }
+    });
+    return resultsCount;
+  }
+
   render() {
     const { children } = this.props;
-    return <typeahead-results-list>{children}</typeahead-results-list>
+    return <typeahead-results-list>
+      {children}
+    </typeahead-results-list>
   }
 }
